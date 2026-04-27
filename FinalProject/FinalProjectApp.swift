9dac9052d8a9a9b60a80d7db0d766162fa1645f6
laura <laura>
2026-04-27 08:30:31 -0500

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by KIRKPATRICK, LAURA on 4/27/26.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
