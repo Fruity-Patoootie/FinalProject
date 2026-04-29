334ffbbd509635c3ace596fc847f77612d7154b0
laura <laura>
2026-04-29 11:17:10 -0500

    Progress

---
+++ FinalProject/CardPickerView.swift
@@ -0,0 +1,18 @@
+//
+//  CardPickerView.swift
+//  FinalProject
+//
+//  Created by KIRKPATRICK, LAURA on 4/29/26.
+//
+
+import SwiftUI
+
+struct CardPickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardPickerView()
+}
