e472e5869c419a8cecf0ddd66254d5d75b5a9862
laura <laura>
2026-05-06 09:58:41 -0500

    Mostly finished. Fixing AsyncImage on first page

---
+++ FinalProject/AboutView.swift
@@ -0,0 +1,18 @@
+//
+//  AboutView.swift
+//  FinalProject
+//
+//  Created by KIRKPATRICK, LAURA on 5/6/26.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AboutView()
+}
---
+++ FinalProject/ApiView.swift
@@ -0,0 +1,18 @@
+//
+//  ApiView.swift
+//  FinalProject
+//
+//  Created by KIRKPATRICK, LAURA on 5/6/26.
+//
+
+import SwiftUI
+
+struct ApiView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ApiView()
+}
